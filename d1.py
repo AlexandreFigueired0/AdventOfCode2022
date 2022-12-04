2ab9242a423062dba08a7f90cd228319f8081c1c
fc57099 <fc57099@alunos.fc.ul.pt>
2022-12-04 14:51:40 +0000

    simplifying day 1

--- d1.py
+++ d1.py
@@ -1,15 +1,5 @@
 f = open("input.txt")
 input = "".join(f.readlines()).split("\n\n")
-formated = list(map(lambda x : list(map(lambda s : int(s),x.split("\n"))),input))
-res = max( map( lambda l : sum(l), formated) )
-print(res)
-
-# Part 2
-res2 = 0
-totalEachCarries = list(map( lambda l : sum(l), formated) )
-for i in range(3):
-    curr_max = max(totalEachCarries )
-    res2 +=  curr_max
-    totalEachCarries.remove(curr_max)
-
-print(res2)
+calories = sorted(list(map(lambda x : sum(list(map(lambda s : int(s),x.split("\n")))),input)), reverse=True)
+print("Part 1:",calories[0])
+print("Part 2:",calories[0] + calories[1] + calories[2])
