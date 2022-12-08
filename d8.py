cb2f20e9fe3c2d8ed98652e80c553aa6d775c841
fc57099 <fc57099@alunos.fc.ul.pt>
2022-12-08 14:37:26 +0000

    Simplifying day8 code

--- d8.py
+++ d8.py
@@ -4,7 +4,7 @@ input = list(map(lambda l : l.strip(),f.readlines()))
 grid = list(map(lambda l : list(map(lambda c : int(c),l) ),input))
 R = len(grid)
 C = len(grid[0])
-DR = [1,0,-1,0]
+DR = [-1,0,1,0]
 DC = [0,1,0,-1]
 res = 2*R + 2*(C-2)
 res2 = 0
@@ -26,59 +26,26 @@ def is_visible(row,col):
                     viewed_from -=1
     return viewed_from >0
 
-def seen_top(row,col):
-    seen = 0
-    dr = -1
+def seen_trees_directions(row,col):
+    seen_dirs_res = [0,0,0,0]
     curr_height = grid[row][col]
-    for scalar in range(1,R):
-        new_row = row + dr * scalar
-        if 0<=new_row<R: #valid pos
-            seen +=1
-            other_height = grid[new_row][col]
-            if other_height >= curr_height: break
-    return seen
-
-def seen_right(row,col):
-    seen = 0
-    dc = 1
-    curr_height = grid[row][col]
-    for scalar in range(1,C):
-        new_col = col + dc * scalar
-        if 0<=new_col<C: #valid pos
-            seen +=1
-            other_height = grid[row][new_col]
-            if other_height >= curr_height: break
-    return seen
-
-def seen_bottom(row,col):
-    seen = 0
-    dr = 1
-    curr_height = grid[row][col]
-    for scalar in range(1,R):
-        new_row = row + dr * scalar
-        if 0<=new_row<R: #valid pos
-            seen +=1
-            other_height = grid[new_row][col]
-            if other_height >= curr_height: break
-    return seen
-
-def seen_left(row,col):
-    seen = 0
-    dc = -1
-    curr_height = grid[row][col]
-    for scalar in range(1,C):
-        new_col = col + dc * scalar
-        if 0<=new_col<C: #valid pos
-            seen +=1
-            other_height = grid[row][new_col]
-            if other_height >= curr_height: break
-    return seen
+    for dir in range(4):
+        dr = DR[dir]
+        dc = DC[dir]
+        for scalar in range(1,max(C,R)):
+            new_row = row + dr * scalar
+            new_col = col + dc * scalar
+            if 0<=new_row<R and 0<=new_col<C: # valid pos
+                seen_dirs_res[dir] +=1
+                other_height = grid[new_row][new_col]
+                if other_height >= curr_height: break
+    return seen_dirs_res
 
 for row in range(1,R-1):
     for col in range(1,C-1):
         if is_visible(row,col): 
             res+=1
-        seen_trees_direction = [seen_top(row,col),seen_right(row,col),seen_bottom(row,col),seen_left(row,col)] # ntrees visible from top,right,bottom,left
+        seen_trees_direction = seen_trees_directions(row,col) # ntrees visible from top,right,bottom,left
         scenic_score = math.prod(seen_trees_direction)
         res2 = max(scenic_score,res2)
 
