acb4372cdd035c8ff2cfe47acbfafe33f88f43c7
fc57099 <fc57099@alunos.fc.ul.pt>
2022-12-12 20:47:26 +0000

    Day12 code improved

--- d12.py
+++ d12.py
@@ -9,13 +9,13 @@ def neighbours(r,c,s):
     res = []
     height = input[r][c]
     for dr,dc in DIR:
-        new_row,new_col = r + dr , c + dc
+        new_row  = r + dr 
+        new_col= c + dc
         if 0<= new_row <R and 0<= new_col < C: #valid pos
             neighbour_height = input[new_row][new_col]
             if neighbour_height - height <= 1:
                 res.append(((new_row,new_col),s+1))
     return res
-
 lowest_points = []
 for r in range(R):
     for c in range(C):
@@ -31,21 +31,19 @@ input[s_pos[0]][s_pos[1]] = start_height
 input[e_pos[0]][e_pos[1]] = end_height
 
 steps = []
-for start_pos in lowest_points:
-    visited = []
-    queue = []
-    visited.append(start_pos)
+visited = []
+queue = []
+for start_pos in lowest_points: # we want to explor all low points
     queue.append((start_pos,0)) # pos and nsteps
-    while queue:
-        pos,s = queue.pop(0)
-        r,c = pos
-        for n in neighbours(r,c,s):
-            if n[0] == e_pos:
-                steps.append(n[1])
-                if start_pos == s_pos:
-                    print("part1",n[1])
-                break
-            if n[0] not in visited:
-                visited.append(n[0])
-                queue.append(n)
-print("part2",min(steps))
+while queue: # for p1 start queue only with s_pos
+    pos,s = queue.pop(0)
+    visited.append(pos) 
+    r,c = pos
+    for n in neighbours(r,c,s):
+        if n[0] == e_pos:
+            steps.append(n[1])
+            print("answer",n[1])
+            first = False
+        if n[0] not in visited:
+            visited.append(n[0])
+            queue.append(n) 
