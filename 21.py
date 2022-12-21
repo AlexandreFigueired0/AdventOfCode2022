dc800331ce62ee5bd5f37a548b6776d71fdbcfae
fc57099 <fc57099@alunos.fc.ul.pt>
2022-12-21 17:22:04 +0000

    cleaning code

--- 21.py
+++ 21.py
@@ -1,7 +1,7 @@
 input = list(map(lambda l : l.strip().split() ,open("input.txt").readlines()))
 monkeys = {}
 for line in input:
-    monkeys[line[0].replace(":","")] = tuple(line[1:]) if len(line[1:]) > 1 else float(line[1])
+    monkeys[line[0].replace(":","")] = tuple(line[1:]) if len(line[1:]) > 1 else int(line[1])
 me = "humn"
 
 def get_monkey_val(monkey):
@@ -17,10 +17,10 @@ def get_monkey_val(monkey):
         return m_action
 min_v = 0
 max_v = 10**13
-mid_v = (min_v+max_v)/2
 p1 = get_monkey_val("root")
 print(int(eval(str(p1[0]) + monkeys["root"][1] + str(p1[1]))))
 while True:
+    mid_v = (min_v+max_v)/2
     monkeys[me] =mid_v
     v1,v2 = get_monkey_val("root")
     if v1 == v2:
@@ -28,8 +28,5 @@ while True:
         break
     elif v1 > v2:
         min_v = mid_v
-        mid_v = (min_v+max_v)/2
     else: 
         max_v = mid_v
-        mid_v = (min_v+max_v)/2
-
